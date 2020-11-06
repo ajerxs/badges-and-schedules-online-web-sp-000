f797eb3db64e6ae3e54553010ab8dd48f626f8e6
Andrew Eames <ajerxs@gmail.com>
2020-11-06 23:03:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+# Write your code here
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+def batch_badge_creator(attendees)
+  list_of_badges = []
+  attendees.each do |badges|
+     list_of_badges << "Hello, my name is #{badges}."
+  end
+  list_of_badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |room, index|
+    room_assignments << "Hello, #{room}! You'll be assigned to room #{index + 1}!"
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+end
